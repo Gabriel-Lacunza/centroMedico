1b6ff5abd7d6dd51cd4a5816346622db44c1dbce
Gabriel Lacunza <69773735+SlimeB@users.noreply.github.com>
2022-11-10 17:39:07 -0300

    feat: presentacion arreglada

--- appMedica/urls.py
+++ appMedica/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("/about", about, name="about"),
-    path("/appointment", appointment, name="appointment"),
-    path("/blog", blog, name="blog"),
-    path("/contact", contact, name="contact"),
-    path("/gallery", gallery, name="gallery"),
+    path("about", about, name="about"),
+    path("appointment", appointment, name="appointment"),
+    path("blog", blog, name="blog"),
+    path("contact", contact, name="contact"),
+    path("gallery", gallery, name="gallery"),
     path("", home, name="home"),
-    path("/services", services, name="services"),
-    path("/team", team, name="team")
+    path("services", services, name="services"),
+    path("team", team, name="team")
 ]
